e10346a4daeebbeb588f41dd5e9cf0f034d99393
raah68 <laughter68@hotmail.com>
2018-02-12 07:18:29 +0000

    link list separated

--- Web Crawler/src/com/raah/objects/PageCrawler.java
+++ Web Crawler/src/com/raah/objects/PageCrawler.java
@@ -18,20 +18,14 @@ import com.raah.webcrawler.main.WebCrawler;
 
 public class PageCrawler {
 	private String _url;
-	private final String _domainName;
+	private String _domainName;
 	private boolean _vistiStatus;
 	private Document _document;
 	private Map<String, String> _linkList = new HashMap<String, String>();
 	private Map<String, String> _imageList  = new HashMap<String, String>();
 	Map<String, String> _externalLinks = new HashMap<String, String>();
 	Map<String, String> _internalLinks = new HashMap<String, String>();
-	
-	public PageCrawler() {
-		this._vistiStatus = true;
-		this._domainName = getDomainNameFromURL(this._url);
-	} //end of constructor
-	
-	
+		
 	/**
 	 * Public method to load a web page using URL 
 	 * @param url requires a web site URL to
@@ -39,7 +33,10 @@ public class PageCrawler {
 	 */
 	public WebPage loadPageDocumentFromURL(String url) {
 		this._url = url;
-		WebPage aPage = null;		
+		this._domainName = getDomainNameFromURL(this._url);
+		this._vistiStatus = true;				
+		WebPage aPage = null;
+		
 		if(connectToURL()){
 			collectAllLinks();
 			separeteInternaLinksFromExternalLinks();			
@@ -49,8 +46,8 @@ public class PageCrawler {
 								_document.title(), 
 								_linkList, 
 								_imageList,
-								_externalLinks, 
-								_internalLinks);
+								_internalLinks, 
+								_externalLinks);
 		}
 		return aPage;
 	} //end of method loadPageDocumentFromURL
@@ -109,13 +106,15 @@ public class PageCrawler {
 	 * Method to separate internal links from external links. 
 	 */
 	private void separeteInternaLinksFromExternalLinks() {
-		for(Entry<String, String> entry : this._linkList.entrySet()) {
-			String entryDomainName = getDomainNameFromURL(entry.getValue());
-			if(entryDomainName.equalsIgnoreCase(this._domainName)) {
-				this._internalLinks.put(entry.getKey(), entry.getValue());
-			} else {
-				this._externalLinks.put(entry.getKey(), entry.getValue());
-			}				
+		for(Entry<String, String> entry : this._linkList.entrySet()) {			
+			if(entry.getValue().startsWith("http")) {
+				String entryDomainName = getDomainNameFromURL(entry.getValue());				
+				if(entryDomainName.equalsIgnoreCase(this._domainName)) {
+					this._internalLinks.put(entry.getKey(), entry.getValue());
+				} else {
+					this._externalLinks.put(entry.getKey(), entry.getValue());
+				}				
+			}
 		}		
 	} //end of method separeteInternaLinksFromExternalLinks
 
@@ -124,14 +123,13 @@ public class PageCrawler {
 	 * @param url requires the URL as String
 	 * @return returns the domain name from the URL
 	 */
-	private static String getDomainNameFromURL(String url) {				
+	public String getDomainNameFromURL(String url) {				
 		try {
 			URL netUrl = new URL(url);
 			return netUrl.getHost();
 		} catch (MalformedURLException e) {			
-			e.printStackTrace();
+			return null;
 		}
-		return null;
 	} //end of method getDomainNameFromURL
 
 } //end of class
--- Web Crawler/src/com/raah/webcrawler/main/WebCrawler.java
+++ Web Crawler/src/com/raah/webcrawler/main/WebCrawler.java
@@ -4,6 +4,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,68 +15,86 @@ public class WebCrawler {
 
 	public static void main(String[] args) {
 		PageCrawler crawler = new PageCrawler();
-		String url = "http://wiprodigital.com/";
-		WebPage firstPage = crawler.loadPageDocumentFromURL(url);
+		//String url = "http://wiprodigital.com/";
+		//WebPage firstPage = crawler.loadPageDocumentFromURL(url);
+		//System.out.println(userInput());
+		//System.out.println(isURL("www.google.com"));
+		
+		String a = "go.com/";
+		String http = "http://";
+		if(!a.contains("://")) {
+			http = http.concat(a);
+			try {
+				URL url = new URL(http);
+				url.toURI();
+				System.out.println(http);
+			} catch (MalformedURLException e) {
+				System.out.println("not a valid URL");				
+			}
+		}
+		
+		
+		
+		
 		
 		//System.out.println(firstPage.get_title());
 		//System.out.println(firstPage.get_url());
 		//System.out.println(firstPage.isVisitStatus());
 		
-//		Map<String, String> pageLinks = firstPage.get_pageLinks();
-//		for(Entry<String, String> entry : pageLinks.entrySet()) {
-//			System.out.println(entry);		
-//		}
+/*		Map<String, String> pageLinks = firstPage.get_pageLinks();
+		for(Entry<String, String> entry : pageLinks.entrySet()) {
+			System.out.println(entry.getValue());		
+		}*/
 		
 		/*Map<String, String> imageList = firstPage.get_imageLinks();
+		 * 
 		System.out.println(imageList.entrySet().size());
 		for(Entry<String, String> entry: imageList.entrySet()) {
 			System.out.println(entry);
 		}*/
 		
-/*		Map<String, String> internalLinks = firstPage.get_internalLinks();
-		System.out.println("List of internal links");
-		for(Entry<String, String> entry : internalLinks.entrySet()) {
-			System.out.println(entry);
-		}*/
+//		Map<String, String> internalLinks = firstPage.get_internalLinks();
+//		System.out.println("List of internal links");
+//		for(Entry<String, String> entry : internalLinks.entrySet()) {
+//			System.out.println(entry);
+//		}
 		
-/*		Map<String, String> externalLinks = firstPage.get_externalLinks();
-		System.out.println("List of internal links");
-		for(Entry<String, String> entry : externalLinks.entrySet()) {
-			System.out.println(entry);
-		}*/
+//		Map<String, String> externalLinks = firstPage.get_externalLinks();
+//		System.out.println("List of internal links");
+//		System.out.println(externalLinks.size());
+//		for(Entry<String, String> entry : externalLinks.entrySet()) {
+//			System.out.println(entry.getValue());
+//		}
 		
-		/*int beginIndex = url.indexOf("://")+3;
-		int endIndex = url.indexOf(".com");
-		System.out.println(url.substring(beginIndex, endIndex));*/
-		//getDomainNameFromURL();
-		System.out.println(getDomainName(url));
-		
-	}
+				
+	} //end of method main
 	
-	public static void getDomainNameFromURL() {
-		String url = "http://google.co.uk/";
-		try {
-			URL netUrl = new URL(url);
-			System.out.println(netUrl.getHost());
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	 
-
-	public static String getDomainName(String url){
-		String hostExtractorRegexString = "(?:https?://)?(?:www\\.)?(.+\\.)(com|au\\.uk|co\\.in|be|in|uk|org\\.in|org|net|edu|gov|mil)";
-		Pattern hostExtractorRegexPattern = Pattern.compile(hostExtractorRegexString);
-		if (url == null) return null;
-	    url = url.trim();
-	    Matcher m = hostExtractorRegexPattern.matcher(url);
-	    if(m.find() && m.groupCount() == 2) {
-	        return m.group(1) + m.group(2);
+	public static URL userInput() {
+		System.out.println("Enter a web site address in the format of http://www. !");
+		Scanner sc = new Scanner(System.in);
+		String inputString = sc.nextLine();
+		URL url = null;
+		if(inputString.length() > 0) {
+				
+			
+			if(isURL(inputString)) {
+				try {
+					url = new URL(inputString);
+				} catch (MalformedURLException e) {
+					System.out.println("Not a valild URL");
+					
+				}
+			} 
+		}										
+		return url;
+	} //end of method userInput
+	
+	public static boolean isURL(String url) {
+		if (url == null) {
+	      return false;
 	    }
-	    else {
-	        return null;
-	    }
+	    String urlPattern = "^http(s{0,1})://[a-zA-Z0-9_/\\-\\.]+\\.([A-Za-z/]{2,5})[a-zA-Z0-9_/\\&\\?\\=\\-\\.\\~\\%]*";
+	    return url.matches(urlPattern);
 	}
 
-}
+} //end of class
